358f6293771e1623215cfee26bf65ec686c2ca7e
Jason Nance <jnance@rti.org>
2017-06-23 12:14:22 -0400

    Rename backend directory; get django container up and running
    Django isn't properly connecting to PG at the moment, though.

---
+++ backend/django/runserver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# wait for postgres to be ready
+nc -z postgres 5432
+n=$?
+while [ $n -ne 0 ]; do
+    sleep 1
+    nc -z postgres 5432
+    n=$?
+done
+
+python ./manage.py runserver 0.0.0.0:8000
