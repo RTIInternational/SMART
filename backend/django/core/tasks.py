07e404913b758a7743811e0a70e1b8592a1a8771
Peter Baumgartner <pbaumgartner@rti.org>
2017-07-11 17:03:33 -0400

    simpler task

--- backend/django/core/tasks.py
+++ backend/django/core/tasks.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import
 from celery import shared_task
-from celery.exceptions import Ignore
-import csv
 
 from .models import Project, Data, Label
 
 @shared_task
 def test():
-    return "Test Task run"
+    return {'result': 'Test Task complete'}
