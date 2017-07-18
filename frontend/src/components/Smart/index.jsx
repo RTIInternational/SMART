209cd5ebf7bf133ec73c55276b3bc8c832aaec8a
Keith Richards <dkrichards@rti.org>
2017-07-18 09:58:28 -0400

    Moves auth to Django instead of React. Being a multi-page app, auth is better handled in Django.

--- frontend/src/components/Smart/index.jsx
+++ frontend/src/components/Smart/index.jsx
@@ -4,58 +4,18 @@ import PropTypes from 'prop-types';
 import { Button } from "react-bootstrap";
 
 import Deck from '../Deck';
-import Header from '../Header';
-import Login from '../Login';
 
-class Smart extends React.Component {
-    render() {
-        let children = null;
-        let headerAside = null;
+const Smart = ({fetchCards, passCard, popCard, cards }) => (
+    <Deck 
+        fetchCards={fetchCards}
+        passCard={passCard}
+        popCard={popCard}
+        cards={cards}
+    />
+);
 
-        if (this.props.token) {
-            children = (
-                <Deck 
-                    fetchCards={this.props.fetchCards}
-                    passCard={this.props.passCard}
-                    popCard={this.props.popCard}
-                    cards={this.props.cards}
-                />
-            );
-
-            headerAside = (
-                <Button 
-                    bsStyle="primary"
-                    type="submit"
-                    onClick={() => this.props.logout()}
-                >
-                    Logout
-                </Button>
-            );
-        }
-        else {
-            children = <Login login={this.props.login} />
-        }
-
-        return (
-           <div>
-                <Header>
-                    {headerAside}
-                </Header>
-                <main>
-                    {children}
-                </main>
-            </div>
-        );
-    }
-};
-
-Header.propTypes = {
-    cards: PropTypes.arrayOf(PropTypes.object),
-    token: PropTypes.string,
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-    ])
+Smart.propTypes = {
+    cards: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default Smart;
--- frontend/src/smart.jsx
+++ frontend/src/smart.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 
 import reducers from './reducers/index.js';
-
+import { handleChange } from './utils/tokens';
 import SmartContainer from './containers/smart_container';
 
 import './styles/smart.scss';
