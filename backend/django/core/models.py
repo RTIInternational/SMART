4ddcb78834908cee49ab88eca4e9cc3c6552c617
Jason Nance <jnance@rti.org>
2017-08-17 11:40:47 -0400

    Make AssignedData unique on user/data as well as user/queue
    This allows us to retrieve the queue for a given combination of user/data,
    which lets us delete DataQueue rows for an assignment when we get it
    back from the front-end with only a label/user/project.

--- backend/django/core/models.py
+++ backend/django/core/models.py
@@ -58,7 +58,7 @@ class DataQueue(models.Model):
 
 class AssignedData(models.Model):
     class Meta:
-        unique_together = (('user', 'queue'))
+        unique_together = (('user', 'queue'), ('user', 'data'))
     user = models.ForeignKey('User')
     data = models.ForeignKey('Data')
     queue = models.ForeignKey('Queue')
