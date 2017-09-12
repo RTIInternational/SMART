0b1211f4156a4ce93e05d6fd27b6643e61d9553d
wengerm50 <mwenger@rti.org>
2017-09-12 13:11:02 -0400

    Create project templates and project urls

---
+++ backend/django/core/urls/projects.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from core.views import frontend
+
+urlpatterns = [
+    url(r'^projects/$', frontend.ProjectList.as_view(), name='project_list'),
+    url(r'^projects/add/$', frontend.ProjectCreate.as_view(), name='project_create'),
+    url(r'^projects/(?P<pk>\d+)/$', frontend.ProjectDetail.as_view(), name='project_detail'),
+    url(r'^projects/(?P<pk>\d+)/update/$', frontend.ProjectUpdate.as_view(), name='project_update'),
+    url(r'^projects/(?P<pk>\d+)/delete/$', frontend.ProjectDelete.as_view(), name='project_delete'),
+]
