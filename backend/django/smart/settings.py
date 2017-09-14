bd22705f1e695d5eec02cb075a51fe3b4f610fcd
Keith Richards <dkrichards@rti.org>
2017-09-14 09:00:23 -0400

    Remove social accounts from allauth. Run 'python manage.py migrate socialaccount zero' to remove the tables.

--- backend/django/smart/settings.py
+++ backend/django/smart/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
     'rest_auth.registration',
     'rest_framework_swagger',
     'webpack_loader'
