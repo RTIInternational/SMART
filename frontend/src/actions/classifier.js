d22f2c05481230bda12577da1f9e3fb112b00715
wengerm50 <mwenger@rti.org>
2017-09-26 15:10:30 -0400

    Update frontend action for new api

--- frontend/src/actions/classifier.js
+++ frontend/src/actions/classifier.js
@@ -14,28 +14,6 @@ export const pushCard = createAction(PUSH_CARD);
 export const setMessage = createAction(SET_MESSAGE);
 
 
-// Get the labels from a project URL
-export const getLabels = (projURL) => {
-    return dispatch => {
-        return fetch(projURL, getConfig())
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                else {
-                    const error = new Error(response.statusText)
-                    error.response = response;
-                    throw error;
-                }
-            })
-            .then(data => {
-                return data['labels']
-            })
-            .catch(err => console.log("Error: ", err));
-    }
-}
-
-
 // Create cards by reading from a queue
 export const fetchCards = (queueID) => {
     // Handle the case where the project has no queue
@@ -46,44 +24,32 @@ export const fetchCards = (queueID) => {
             dispatch(setMessage(message));
         }
     }
-    else {
-        let apiURL = `/api/queue/${queueID}/`;
-        return dispatch => {
-            return fetch(apiURL, getConfig())
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
+    let apiURL = `/api/grab_from_queue/${queueID}/`;
+    return dispatch => {
+        return fetch(apiURL, getConfig())
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    const error = new Error(response.statusText)
+                    error.response = response;
+                    throw error;
+                }
+            })
+            .then(response => {
+                // If error was in the response then set that message
+                if ('error' in response) return dispatch(setMessage(response.error));
+
+                for (let i = 0; i < response.data.length; i++) {
+                    const card = {
+                        id: i,
+                        options: response.labels,
+                        text: response.data[i]
                     }
-                    else {
-                        const error = new Error(response.statusText)
-                        error.response = response;
-                        throw error;
-                    }
-                })
-                .then(data => {
-                    // Make sure the queue is not empty
-                    if (data.data.length === 0) {
-                        let message = 'There is nothing in the queue.  Please check again later.';
-                        return dispatch(setMessage(message));
-                    }
-                    // Now that we have queue data we still need the labels data from project
-                    let labels = dispatch(getLabels(data['project'])).then(labels => {
-                        // If the project has no labels then do not create cards
-                        if (labels.length === 0) {
-                            let message = 'There are no labels for this project. Please have your administator create labels.';
-                            return dispatch(setMessage(message));
-                        }
-                        for (let i = 0; i < data.data.length; i++) {
-                            const card = {
-                                id: i,
-                                options: labels,
-                                text: data.data[i]
-                            }
-                            dispatch(pushCard(card));
-                        }
-                    })
-                })
-                .catch(err => console.log("Error: ", err));
-        }
+                    dispatch(pushCard(card));
+                }
+            })
+            .catch(err => console.log("Error: ", err));
     }
 };
