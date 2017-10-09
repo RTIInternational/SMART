5900b895dbb7ed03825d00098551e81fcfc52340
wengerm50 <mwenger@rti.org>
2017-10-09 16:10:33 -0400

    Sync redis queues at startup

---
+++ backend/django/core/__init__.py
@@ -0,0 +1,2 @@
+# This will set the default config to core, where we have our startup code
+default_app_config = 'core.apps.CoreConfig'
--- backend/django/core/apps.py
+++ backend/django/core/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        from .util import sync_redis_queues
+
+
+        print('configstartup')
+
+        sync_redis_queues()
