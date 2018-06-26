b49ebd88991007f949f2b198a9f86457f0235dd8
Kery <ckery@RTI-101849.rti.ns>
2018-06-26 09:56:54 -0400

    added a progress bar #7 and creator is shown as an admin #78

--- frontend/src/components/Smart/index.jsx
+++ frontend/src/components/Smart/index.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { ProgressBar } from "react-bootstrap";
 import Deck from '../Deck';
 
-const Smart = ({fetchCards, annotateCard, passCard, popCard, cards, message }) => (
-    <Deck 
-        fetchCards={fetchCards}
-        annotateCard={annotateCard}
-        passCard={passCard}
-        popCard={popCard}
-        cards={cards}
-        message={message}
-    />
-);
+const Smart = ({fetchCards, annotateCard, passCard, popCard, cards, message }) => {
+  var progress = 0;
+  if(cards.length > 0)
+  {
+      progress = (cards[0].id/cards[cards.length-1].id) * 100;
+  }
+  return(
+    <div>
+      <Deck
+          fetchCards={fetchCards}
+          annotateCard={annotateCard}
+          passCard={passCard}
+          popCard={popCard}
+          cards={cards}
+          message={message}
+      />
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <ProgressBar now={progress}/>
+    </div>
+    );
+}
 
 Smart.propTypes = {
     cards: PropTypes.arrayOf(PropTypes.object),
