784b905563b4c0f058c3dc36f03d6ce56ab2ad9c
Kery <ckery@RTI-101849.rti.ns>
2018-07-06 11:17:27 -0400

    changed the rendering so now only the top card shows as per issue #76

--- frontend/src/components/Smart/index.jsx
+++ frontend/src/components/Smart/index.jsx
@@ -1,22 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, ButtonToolbar, Clearfix, Well, Tooltip, OverlayTrigger } from "react-bootstrap";
+import Card from '../Card';
+class Smart extends React.Component {
 
-import Deck from '../Deck';
+  componentWillMount() {
+      this.props.fetchCards();
+  }
 
-const Smart = ({fetchCards, annotateCard, passCard, popCard, cards, message }) => (
-    <Deck 
-        fetchCards={fetchCards}
-        annotateCard={annotateCard}
-        passCard={passCard}
-        popCard={popCard}
-        cards={cards}
-        message={message}
-    />
-);
+  render(){
+    const { message, cards, passCard, annotateCard } = this.props;
+    const cardCount = cards.length;
+    if (cardCount > 0) {
+      var card = (
+      <Card className="full" key={cards[0].id}>
+          <h2>Card {cards[0].id + 1}</h2>
+          <p>
+              { cards[0].text['text'] }
+          </p>
+          <ButtonToolbar bsClass="btn-toolbar pull-right">
+              {cards[0].options.map( (opt) => (
+                  <Button onClick={() => annotateCard(cards[0], opt['pk'])} bsStyle="primary" key={`deck-button-${opt['name']}`}>{opt['name']}</Button>
+              ))}
+              <OverlayTrigger
+              placement = "top"
+              overlay={
+                <Tooltip id="skip_tooltip">
+                  Clicking this button will send this document to an administrator for review
+                </Tooltip>
+              }>
+                <Button onClick={() => passCard(cards[0])} bsStyle="info">Skip</Button>
+              </OverlayTrigger>
+          </ButtonToolbar>
+          <Clearfix />
+      </Card>);
+    }
+    else {
+        let blankDeckMessage = (message) ? message : "No more data to label at this time. Please check back later";
+        card = (
+            <Well bsSize="large">
+                { blankDeckMessage }
+            </Well>
+        );
+    }
 
+    return (
+        <div className="deck">
+            {card}
+        </div>
+    );
+
+  };
+}
 Smart.propTypes = {
-    cards: PropTypes.arrayOf(PropTypes.object),
-    message: PropTypes.string
+  fetchCards: PropTypes.func.isRequired,
+  annotateCard: PropTypes.func.isRequired,
+  passCard: PropTypes.func.isRequired,
+  popCard: PropTypes.func.isRequired,
+  cards: PropTypes.arrayOf(PropTypes.object),
+  message: PropTypes.string
 };
 
 export default Smart;
