520ba475c53efecc4373384a3d26929f1332166c
wengerm50 <mwenger@rti.org>
2018-09-04 14:12:49 -0400

    refactor historyTable react page, show non-changle alert for irr, disable same label button

--- frontend/src/components/HistoryTable/index.jsx
+++ frontend/src/components/HistoryTable/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
-import { Button, ButtonToolbar, Tooltip, OverlayTrigger } from "react-bootstrap";
+import { Button, ButtonToolbar, Tooltip, OverlayTrigger, Alert } from "react-bootstrap";
 import CodebookLabelMenu from '../CodebookLabelMenu';
-const columns = [
+
+const COLUMNS = [
     {
         Header: "edit",
         accessor: "edit",
@@ -50,9 +51,72 @@ class HistoryTable extends React.Component {
         this.props.getHistory();
     }
 
+    getLabelButton(row, label) {
+        const { changeLabel } = this.props;
+
+        if (row.row.old_label_id === label.pk) {
+            return (
+                <Button
+                    key={label.pk.toString() + "_" + row.row.id.toString()}
+                    bsStyle="primary"
+                    disabled>
+                    {label.name}
+                </Button>
+            );
+        } else {
+            return (
+                <Button
+                    key={label.pk.toString() + "_" + row.row.id.toString()}
+                    onClick={() => changeLabel(row.row.id, row.row.old_label_id, label.pk)}
+                    bsStyle="primary">
+                    {label.name}
+                </Button>
+            );
+        }
+    }
+
+    getSubComponent(row) {
+        let subComponent;
+        const { labels, changeToSkip } = this.props;
+
+        if (row.row.edit === "yes") {
+            subComponent = (
+                <div className="sub-row">
+                    <p>{row.row.data}</p>
+                    <ButtonToolbar bsClass="btn-toolbar pull-right">
+                        {labels.map( (label) => this.getLabelButton(row, label) )}
+                        <OverlayTrigger
+                            placement = "top"
+                            overlay={
+                                <Tooltip id="skip_tooltip">
+                                    Clicking this button will send this document to an administrator for review
+                                </Tooltip>
+                            }>
+                            <Button onClick={() => changeToSkip(row.row.id, row.row.old_label_id)}
+                                bsStyle="info">
+                                Skip
+                            </Button>
+                        </OverlayTrigger>
+                    </ButtonToolbar>
+                </div>
+            );
+        } else {
+            subComponent = (
+                <div className="sub-row">
+                    <p>{row.row.data}</p>
+                    <Alert bsStyle="warning">
+                        <strong>Note:</strong>
+                         This is Inter-rater Reliability data and is not editable.
+                    </Alert>
+                </div>
+            );
+        }
+
+        return subComponent;
+    }
 
     render() {
-        const { history_data, labels, changeLabel, changeToSkip } = this.props;
+        const { history_data, labels } = this.props;
 
         let page_sizes = [1];
         let counter = 1;
@@ -68,67 +132,22 @@ class HistoryTable extends React.Component {
                 <p>This page allows a coder to change past labels.</p>
                 <p>To annotate, click on a data entry below and select the label from the expanded list of labels. The chart will then update with the new label and current timestamp </p>
                 <p><strong>NOTE:</strong> Data labels that are changed on this page will not effect past model accuracy or data selected by active learning in the past. The training data will only be updated for the next run of the model</p>
-                <CodebookLabelMenu
-                    labels={labels}
-                />
+                <CodebookLabelMenu labels={labels} />
                 <ReactTable
                     data={history_data}
-                    columns={columns}
+                    columns={COLUMNS}
                     pageSize={(history_data.length < 50) ? history_data.length : 50}
                     showPageSizeOptions={false}
-                    SubComponent={row => {
-
-                        if (row.row.edit === "yes") {
-                            return (
-                                <div className="sub-row">
-                                    <p>{row.row.data}</p>
-                                    <ButtonToolbar bsClass="btn-toolbar pull-right">
-                                        {labels.map( (label) => {
-                                            return (
-                                                <Button key={label.pk.toString() + "_" + row.row.id.toString()}
-                                                    onClick={() => {
-                                                        if (!(row.row.old_label_id === label.pk)) {
-                                                            changeLabel(row.row.id, row.row.old_label_id, label.pk);
-                                                        }
-                                                    }}
-                                                    bsStyle="primary"
-                                                >{label.name}</Button>
-                                            );
-                                        })}
-                                        <OverlayTrigger
-                                            placement = "top"
-                                            overlay={
-                                                <Tooltip id="skip_tooltip">
-                  Clicking this button will send this document to an administrator for review
-                                                </Tooltip>
-                                            }>
-                                            <Button onClick={() => changeToSkip(row.row.id, row.row.old_label_id)}
-                                                bsStyle="info"
-                                            >Skip</Button>
-                                        </OverlayTrigger>
-                                    </ButtonToolbar>
-                                </div>
-                            );
-                        } else {
-                            return (
-                                <div className="sub-row">
-                                    <p>{row.row.data}</p>
-                                    <p id="irr_history_message">Note: This is Inter-rater Reliability data and is not editable.</p>
-                                </div>
-                            );
-                        }
-                    }}
+                    SubComponent={row => this.getSubComponent(row)}
                     filterable={true}
                     defaultSorted={[{
                         id: "timestamp",
                         desc: true
                     }]}
-
                 />
             </div>
         );
     }
-
 }
 
 
