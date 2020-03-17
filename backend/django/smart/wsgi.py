041818c3ed3c004598413ba8de4c826c0be4d939
wengerm50 <mwenger@rti.org>
2020-03-17 12:53:28 -0400

    Restores incorrectly deleted environ setting in wsgi file

--- backend/django/smart/wsgi.py
+++ backend/django/smart/wsgi.py
@@ -5,7 +5,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
+import os
 
 from configurations.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smart.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
 
 application = get_wsgi_application()
