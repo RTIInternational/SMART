ecd87eaea1fefc4d996cb96039954e10b89eacad
Durk Steed <dsteed@rti.org>
2021-07-12 11:40:16 -0400

    add lint scripts and cfgs

---
+++ backend/django/scripts/isort.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Helper script for running isort formatter
+# By default this script will run in --check mode
+#   if --format is passed then script will format code
+
+echo "hello"
+# set -euo pipefail
+# IFS=$'\n\t'
+
+# if [[ $* == --format ]]; then
+#   isort -rc /code/
+# else
+#   set +e
+#   isort -rc --check-only /code/
+#   exitcode=$?
+#   set -e
+
+#   if [ $exitcode -ne 0 ]; then
+#     echo ""
+#     echo "Fix the above isort errors before running tests"
+#     exit 1
+#   fi
+# fi
