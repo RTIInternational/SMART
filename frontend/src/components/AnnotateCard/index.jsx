b7d13fdfbf5693b47d6ce66ad28b5f85b81ce489
Jason DuPertuis <jdupertuis@rti.org>
2021-09-23 17:04:13 -0400

    recycle bin uses AnnotateCard

--- frontend/src/components/AnnotateCard/index.jsx
+++ frontend/src/components/AnnotateCard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import {
     Button,
     ButtonToolbar,
@@ -10,87 +10,111 @@ import Select from "react-dropdown-select";
 import CardData from '../DataCard/CardData';
 import SuggestedLabels from '../DataCard/SuggestedLabels';
 
-export default function AnnotateCard({ card, labels, onSelectLabel, onSkip = null, onDiscard = null }) {
-    const labelsOptions = useMemo(() => labels.map(label => ({
-        value: label["pk"],
-        dropdownLabel: `${label["name"]} ${label["description"] !== '' ? '(' + label["description"] + ')' : ''}`
-    })));
-
+export default function AnnotateCard({ card, labels, onSelectLabel, readonly = false, onSkip = null, onDiscard = null }) {
     return (
         <div className="cardface clearfix">
             <div className="cardface-datacard">
                 <CardData card={card} />
-                <SuggestedLabels card={card} labels={labels} />
+                { !readonly && <SuggestedLabels card={card} labels={labels} /> }
             </div>
             
-            <ButtonToolbar className="btn-toolbar">
-                {labels.length > 5 ? (
-                    <Select
-                        className="align-items-center flex py-1 px-2"
-                        dropdownHandle={false}
-                        labelField="dropdownLabel"
-                        onChange={(value) => {
-                            onSelectLabel(card, value[0].value);
-                        }}
-                        options={labelsOptions}
-                        placeholder="Select label..."
-                        searchBy="dropdownLabel"
-                        sortBy="dropdownLabel"
-                    />
-                ) : (
-                    labels.map(opt => (
-                        <Button
-                            onClick={() => onSelectLabel(card, opt["pk"])}
-                            variant="primary"
-                            key={`deck-button-${opt["name"]}`}
-                        >
-                            {opt["name"]}
-                        </Button>
-                    ))
-                )}
-                {
-                    onSkip != null &&
-                    <OverlayTrigger
-                        placement="top"
-                        overlay={
-                            <Tooltip id="skip_tooltip">
-                                Clicking this button will send this
-                                document to an administrator for review
-                            </Tooltip>
-                        }
-                    >
-                        <Button
-                            className="ajucate-button"
-                            onClick={() => onSkip(card)}
-                            variant="info"
-                        >
-                            Adjudicate
-                        </Button>
-                    </OverlayTrigger>
-                }
-                {
-                    onDiscard != null &&
-                    <OverlayTrigger
-                        placement="top"
-                        overlay={
-                            <Tooltip id="discard_tooltip">
-                                This marks this data as uncodable,
-                                and will remove it from the active
-                                data in this project.
-                            </Tooltip>
-                        }
-                    >
-                        <Button
-                            key={`discard_${card.id}`}
-                            onClick={() => onDiscard(card.id)}
-                            variant="danger"
-                        >
-                            Discard
-                        </Button>
-                    </OverlayTrigger>
-                }
-            </ButtonToolbar>
+            { !readonly && drawEditOptions(card, labels, onSelectLabel, onSkip, onDiscard) }
         </div>
+    );
+}
+
+function drawEditOptions(card, labels, onSelectLabel, onSkip, onDiscard) {
+    const labelsOptions = labels.map(label => ({
+        value: label["pk"],
+        dropdownLabel: `${label["name"]} ${label["description"] !== '' ? '(' + label["description"] + ')' : ''}`
+    }));
+
+    return (
+        <ButtonToolbar className="btn-toolbar">
+            {labels.length > 5 ? (
+                drawLabelSelect(card, labelsOptions, onSelectLabel)
+            ) : (
+                drawLabelButtons(card, labels, onSelectLabel)
+            )}
+            { onSkip != null && drawSkipButton(card, onSkip) }
+            { onDiscard != null && drawDiscardButton(card, onDiscard) }
+        </ButtonToolbar>
+    );
+}
+
+function drawLabelSelect(card, labelsOptions, onSelectLabel) {
+    return (
+        <Select
+            className="align-items-center flex py-1 px-2"
+            dropdownHandle={false}
+            labelField="dropdownLabel"
+            onChange={(value) => {
+                onSelectLabel(card, value[0].value);
+            }}
+            options={labelsOptions}
+            placeholder="Select label..."
+            searchBy="dropdownLabel"
+            sortBy="dropdownLabel"
+        />
+    );
+}
+
+function drawLabelButtons(card, labels, onSelectLabel) {
+    return (
+        labels.map(opt => (
+            <Button
+                onClick={() => onSelectLabel(card, opt["pk"])}
+                variant="primary"
+                key={`deck-button-${opt["name"]}`}
+            >
+                {opt["name"]}
+            </Button>
+        ))
+    );
+}
+
+function drawSkipButton(card, onSkip) {
+    return (
+        <OverlayTrigger
+            placement="top"
+            overlay={
+                <Tooltip id="skip_tooltip">
+                    Clicking this button will send this
+                    document to an administrator for review
+                </Tooltip>
+            }
+        >
+            <Button
+                className="ajucate-button"
+                onClick={() => onSkip(card)}
+                variant="info"
+            >
+                Adjudicate
+            </Button>
+        </OverlayTrigger>
+    );
+}
+
+function drawDiscardButton(card, onDiscard) {
+    return (
+        <OverlayTrigger
+            placement="top"
+            overlay={
+                <Tooltip id="discard_tooltip">
+                    This marks this data as uncodable,
+                    and will remove it from the active
+                    data in this project.
+                </Tooltip>
+            }
+        >
+            <Button
+                key={`discard_${card.id}`}
+                onClick={() => onDiscard(card.id)}
+                variant="danger"
+            >
+                Discard
+            </Button>
+        </OverlayTrigger>
     );
 }
 
