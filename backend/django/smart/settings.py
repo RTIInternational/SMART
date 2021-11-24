b572c7e8999e123cd21cbe495d30bc3304ecc42f
ckery_rtiintl <ckery@rti.org>
2021-11-24 12:58:18 -0500

    add auto field to settings to account for change in django

--- backend/django/smart/settings.py
+++ backend/django/smart/settings.py
@@ -16,6 +16,8 @@ from configurations import Configuration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
