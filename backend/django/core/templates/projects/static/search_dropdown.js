dc44d4e9be90a454116d063fa6e944f401da3ee2
schittarath3-rti <schittarath.contractor@rti.org>
2022-07-18 15:09:06 -0400

    searchable dropdowns for perm profiles

---
+++ backend/django/core/templates/projects/static/search_dropdown.js
@@ -0,0 +1,18 @@
+$("#permSelect").ready(function(){
+    $("select[id$='profile']").addClass("searchSelect").select2({
+        width: "100%"
+    })
+})
+
+$("#permSelect").on("DOMNodeInserted", "tr", function() {
+    setTimeout( function() {
+    console.log("clicked")
+    $("select[id$='profile']").each(function() {
+        if (!$(this).hasClass("searchSelect")) {
+        $(this).addClass("searchSelect").select2({
+            width: "100%"
+        })
+        }
+    })
+    }, 0.1) // Need to wait for all nodes to be inserted
+})
