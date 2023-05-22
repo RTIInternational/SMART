2249a499eaa903661a406c4b048e477f72fdd127
Durk Steed <dsteed@rti.org>
2023-05-22 08:57:21 -0400

    add QueryClientProvider

--- frontend/src/smart.jsx
+++ frontend/src/smart.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "./styles/index.scss";
+import "./styles/smart.scss";
 
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
+import { QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import reducers from './reducers/index.js';
-import SmartContainer from './containers/smart_container';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
 
-import './styles/smart.scss';
+import SmartContainer from "./containers/smart_container";
+import reducers from "./reducers/index.js";
+import { queryClient } from "./store.js";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(
@@ -16,8 +19,10 @@ const store = createStore(reducers, composeEnhancers(
 ));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <SmartContainer />
-    </Provider>,
-    document.getElementById('mount')
+    <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+            <SmartContainer />
+        </Provider>
+    </QueryClientProvider>,
+    document.getElementById("mount")
 );
