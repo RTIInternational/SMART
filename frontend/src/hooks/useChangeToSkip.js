9551f54a33d46af5e3c3028890fbb8e20db435a6
Durk Steed <dsteed@rti.org>
2023-05-22 08:58:34 -0400

    create useChangeToSkip

---
+++ frontend/src/hooks/useChangeToSkip.js
@@ -0,0 +1,15 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { PROJECT_ID, queryClient } from "../store";
+import { postConfig } from "../utils/fetch_configs";
+
+const useChangeToSkip = () =>
+    useMutation({
+        mutationFn: ({ dataID, message, oldLabelID }) =>
+            fetch(`/api/modify_label_to_skip/${dataID}/`, postConfig({ dataID, message, oldLabelID })),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["history", PROJECT_ID] });
+        }
+    });
+
+export default useChangeToSkip;
