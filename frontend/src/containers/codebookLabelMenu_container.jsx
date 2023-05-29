eebf44023119691c789b395ad060247828b60df0
Caroline Kery <ckery@rti.org>
2023-05-29 14:52:39 -0400

    move labels to smart component and fix dependency issue with node-sass

--- frontend/src/containers/codebookLabelMenu_container.jsx
+++ frontend/src/containers/codebookLabelMenu_container.jsx
@@ -7,7 +7,7 @@ const CodebookLabelMenuContainer = (props) => <CodebookLabelMenu {...props} />;
 
 const mapStateToProps = (state) => {
     return {
-        labels: state.card.labels
+        labels: state.smart.labels
     };
 };
 
